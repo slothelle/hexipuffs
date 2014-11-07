ed63fab412ffa1936779a64010ade4e9a1765b55
Liz Abinante <eabinante@gmail.com>
2014-11-06 18:45:25 -0800

    it is ALIVE

--- app.rb
+++ app.rb
@@ -1,10 +1,10 @@
-class App < Sinatra::Base
-  register Sinatra::AssetPack
-  assets do
-    js :application, ['js/*.js']
-    css :application, ['/css/*.sass']
-    css_compression :sass
-  end
+register Sinatra::AssetPack
+assets do
+  js :application, ['js/*.js']
+  css :application, ['/css/*.sass']
+  css_compression :sass
+end
 
-  # Rest of your app
+get '/' do
+  haml :index
 end
